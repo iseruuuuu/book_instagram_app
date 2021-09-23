b8c957bdd8cb2040afe7e6090e1841482442797b
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2021-09-23 22:42:25 +0900

    add: appbarを修正

--- lib/screen/register/children/register_app_bar.dart
+++ lib/screen/register/children/register_app_bar.dart
@@ -6,28 +6,42 @@ class RegisterAppBar extends StatelessWidget with PreferredSizeWidget {
   }) : super(key: key);
 
   @override
-  Size get preferredSize => const Size.fromHeight(kToolbarHeight + 1);
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
 
   @override
   Widget build(BuildContext context) {
     return AppBar(
       elevation: 1,
-      backgroundColor: Colors.red,
+      backgroundColor: Colors.white,
       title: const Text(
         '新規投稿',
         style: TextStyle(color: Colors.black),
       ),
       automaticallyImplyLeading: true,
+      leading: GestureDetector(
+        onTap: () {
+          Navigator.pop(context);
+        },
+        child: const Icon(
+          Icons.arrow_back,
+          size: 30,
+          color: Colors.black,
+        ),
+      ),
       actions: [
         Padding(
-          padding: const EdgeInsets.only(right: 10),
-          child: TextButton(
-            onPressed: () {},
+          padding: const EdgeInsets.all(10),
+          child: GestureDetector(
+            onTap: () {
+              //値を渡してあげる。
+              Navigator.pop(context);
+            },
             child: const Text(
               '投稿',
               style: TextStyle(
                 fontSize: 20,
                 fontWeight: FontWeight.bold,
+                color: Colors.blue,
               ),
             ),
           ),
