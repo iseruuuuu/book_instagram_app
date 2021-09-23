8f57a402718c391eac1971cdf03ec5f258cfad34
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2021-09-23 14:31:24 +0900

    add: add key

--- lib/screen/timeline/feed_screen.dart
+++ lib/screen/timeline/feed_screen.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import '../../component/bottom_appbar.dart';
 
 class FeedScreen extends StatefulWidget {
+  const FeedScreen({Key? key}) : super(key: key);
+
   @override
   _FeedScreenState createState() => _FeedScreenState();
 }
