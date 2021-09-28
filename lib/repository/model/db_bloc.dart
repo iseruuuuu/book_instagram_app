df25302a997e44534f23de460689d6a1da001cec
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2021-09-28 17:30:35 +0900

    add: blocを追加

--- lib/repository/model/db_bloc.dart
+++ lib/repository/model/db_bloc.dart
@@ -1,26 +1,41 @@
 import 'dart:async';
 import 'package:book_instagram_app/repository/model.dart';
+import 'package:book_instagram_app/repository/model/provider.dart';
 
 class DBBloc {
-  final _dBController = StreamController<List<Todo>>();
+  final _todoController = StreamController<List<Todo>>();
+  Stream<List<Todo>> get todoStream => _todoController.stream;
 
-  Stream<List<Todo>> get DBstream => _dBController.stream;
-
-  getDB() async {
-    _dBController.sink.add(await DBProvider.db.getAllTodos());
+  getTodos() async {
+    _todoController.sink.add(await DBProvider.db.getAllTodos());
   }
 
-  DBBloc() {
-    getDB();
+  TodoBloc() {
+    getTodos();
   }
 
   dispose() {
-    //多くのアプリケーションは1つのデータベースを使用し、データベースを閉じる必要はありません（アプリケーションが終了するとデータベースは閉じられます）。リソースを解放したい場合は、データベースを閉じることができます。
-    _dBController.close();
+    _todoController.close();
   }
 
   create(Todo todo) {
     todo.assignUUID();
+    DBProvider.db.createTodo(todo);
+    getTodos();
   }
 
+  update(Todo todo) {
+    DBProvider.db.updateTodo(todo);
+    getTodos();
+  }
+
+  delete(String id) {
+    DBProvider.db.deleteTodo(id);
+    getTodos();
+  }
+
+  allDelete() {
+    DBProvider.db.deleteAllTodo();
+    getTodos();
+  }
 }
